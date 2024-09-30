da5153ca775a45673b87cddec57b02f48d58d2ec
Ivan Komissarov <ivan.komissarov@coople.com>
2024-09-30 20:19:17 +0400

    Add lambda

--- src/main/java/com/myorg/MyPipelineStack.java
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -2,8 +2,10 @@ package com.myorg;
 
 import java.util.Arrays;
 import software.constructs.Construct;
+import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.StageProps;
 import software.amazon.awscdk.pipelines.CodePipeline;
 import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.pipelines.ShellStep;
@@ -23,5 +25,12 @@ public class MyPipelineStack extends Stack {
                                .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
                                .build())
                 .build();
+
+        pipeline.addStage(new MyPipelineAppStage(this, "natosha", StageProps.builder()
+                .env(Environment.builder()
+                             .account("266735842067")
+                             .region("eu-west-1")
+                             .build())
+                .build()));
     }
 }
